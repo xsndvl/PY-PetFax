ad898de6d0edf94f40b15a62891a55721c6687c1
Joswag Sandoval <sergiojr100@outlook.com>
2022-05-07 03:00:18 -0500

    flask

---
+++ app.py
@@ -0,0 +1,21 @@
+# config
+from flask import Flask
+app = Flask(__name__)
+
+# index route
+@app.route("/")
+def index():
+    return("Hello, this is PetFax")
+
+# pets route
+@app.route("/pets")
+def pets():
+    return("These are our pets available for adoption!")
+
+@app.route("/pets/<pet>")
+def show_pets(pet):
+    return(f"This pet is a(n) {pet} and here are some fun facts about it!")
+
+@app.route("/facts")
+def facts():
+    return("This is the fact page! here are some facts about the pets!")
